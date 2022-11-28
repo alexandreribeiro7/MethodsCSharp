e7edb7650ed263c16a54d4e9689954a2923b08e4
Seu nome completo entre áspas <seuemail@algumacoisa.com>
2022-11-28 20:04:29 -0300

    CPF validation with regex

---
+++ Regex/Regex.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+Console.Write("Informe um cpf para validação: ");
+var cpf = Console.ReadLine();
+
+Regex regex = new Regex(@"([0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})", RegexOptions.IgnorePatternWhitespace);
+if (regex.IsMatch(cpf))
+{
+    Console.WriteLine("CPF válido.");
+}
+else
+{
+    Console.WriteLine("CPF inválido.");
+}
